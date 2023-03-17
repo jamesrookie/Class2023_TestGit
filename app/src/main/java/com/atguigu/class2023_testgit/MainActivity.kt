9b25f4ae21d848accdcb5098cdb7fc96c87db289
rookie james <Y135246135246@gmail.com>
2023-03-17 23:02:10 +0800

    implement: Button Add onClick Listener

--- app/src/main/java/com/atguigu/class2023_testgit/MainActivity.kt
+++ app/src/main/java/com/atguigu/class2023_testgit/MainActivity.kt
@@ -2,10 +2,17 @@ package com.atguigu.class2023_testgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.atguigu.class2023_testgit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private var number:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnAdd.setOnClickListener {
+            binding.tvNumber.text="${number++}"
+        }
     }
 }
