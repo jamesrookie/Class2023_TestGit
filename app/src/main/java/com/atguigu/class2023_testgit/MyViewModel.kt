94ba7924f1b09b518e53ed4e0daf83011b448fc0
rookie james <Y135246135246@gmail.com>
2023-03-18 20:47:15 +0800

    refactor: use LiveDate in ViewModel

--- app/src/main/java/com/atguigu/class2023_testgit/MyViewModel.kt
+++ app/src/main/java/com/atguigu/class2023_testgit/MyViewModel.kt
@@ -1,7 +1,17 @@
 package com.atguigu.class2023_testgit
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel:ViewModel() {
+    private val _number=MutableLiveData(0)
+    val numberLiveData: LiveData<Int> get()=_number
+    fun add(n:Int){
+        _number.value= _number.value?.plus(1)
+    }
+    fun reset(){
+        _number.value=0
+    }
     var number=0
 }
